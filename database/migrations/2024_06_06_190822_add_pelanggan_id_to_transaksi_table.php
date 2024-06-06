567110292d54900c4ad2d23637b7ca22b01d7cfb
Fauzan Razendrya Azhar <fauzan.azhar@binus.ac.id>
2024-06-07 02:12:36 +0700

    commit_add_idpelanggan_to_transaksi_table

---
+++ database/migrations/2024_06_06_190822_add_pelanggan_id_to_transaksi_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transaksi', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_pelanggan');
+            $table->foreign('id_pelanggan')->references('id')->on('pelanggan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transaksi', function (Blueprint $table) {
+            $table->dropForeign('transaksi_id_pelanggan_foreign');
+            $table->dropColumn('id_pelanggan');
+        });
+    }
+};
